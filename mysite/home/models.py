4fa18964b85fe7d5a3457b6ae3a62ac384ca5dda
Rakibul islam <33988342+raykibul@users.noreply.github.com>
2021-01-18 19:22:09 +0600

    messages and home contact model created

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    sno=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=255)
+    email=models.CharField(max_length=120)
+    message=models.TextField(max_length=255)
+    timeStamp=models.DateTimeField(auto_now_add=True,blank=True)
+
+    def __str__(self):
+        return 'Name:  '+self.name+' ---Message:  '+self.message+'--- Email: '+self.email
+ 
+
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render,HttpResponse
-
+from home.models import Contact
+from django.contrib import messages
 # Create your views here.
 def home(index):
     return render( index,'home/home.html')
 
-def contact(index):
-    return render(index,'home/contact.html')
+def contact(request):
+    if request.method=='POST':
+        name=request.POST['inputName']
+        email=request.POST['inputEmail']
+        message=request.POST['message']
+        contact=Contact(name=name,email=email,message=message)
+        if len(name)<2 or len(email)<3 or len(message)<4:
+            messages.error(request,"Please Fill the Form Correctly!!")
+        else:
+            messages.success(request,"Thank You !! Your Message has been sent!")
+            contact.save()
+    return render(request,'home/contact.html')
 
-def about(index):
-    return render(index,'home/about.html')
+def about(request):
+    return render(request,'home/about.html')
 
